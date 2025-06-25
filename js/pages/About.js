adadb04731067dffbb4fd8557c22b1b4cad6e0e0
Cluckish <ethan.m.moore0@gmail.com>
2025-06-25 16:41:14 -0600

    Update About.js

--- js/pages/About.js
+++ js/pages/About.js
@@ -9,7 +9,7 @@ export function About() {
                         <p>Benoit Marketing Consultants began as a one-person operation out of a college apartment in Moscow, Idaho. While studying marketing at the University of Idaho, our founder, Mason Benoit, was inspired to apply what he was learning to real businesses.</p>
                     </div>
                     <div class="card-image">
-                        <img src="images/about-apartment.png" alt="University of Idaho campus building" />
+                        <img src="images/Appt-desk.jpg" alt="Desk setup in an apartment" />
                     </div>
                 </div>
             </section>
@@ -22,7 +22,7 @@ export function About() {
                         <a href="https://www.instagram.com/uofi_entrepreneurship/" class="cta-button" target="_blank" rel="noopener noreferrer">Check Them Out!</a>
                     </div>
                     <div class="card-image">
-                        <img src="images/about-e-club.png" alt="Founder Mason Benoit speaking" />
+                        <img src="images/podium.jpg" alt="Founder Mason Benoit speaking at a University of Idaho podium" />
                     </div>
                 </div>
             </section>
@@ -30,12 +30,12 @@ export function About() {
             <section class="about-section dark-bg">
                 <div class="history-grid">
                     <div class="history-card large-card">
-                        <img src="images/about-peoples-pretzel.jpg" alt="The People's Pretzel stand" />
+                        <img src="images/pretzel-cart.jpg" alt="The People's Pretzel team" />
                         <h3>The People's Pretzel</h3>
                         <p>In his senior year, Mason launched his first business — The People’s Pretzel — which empowered college students by selling franchising rights to run pretzel stands on campus.</p>
                     </div>
                     <div class="history-card small-card">
-                        <img src="images/about-bmc-gallery.jpg" alt="Founder Mason Benoit" />
+                        <img src="images/ENTR-club.jpg" alt="University of Idaho Entrepreneurship Club Logo" />
                         <h3>Benoit Marketing Consultants</h3>
                         <p>After graduating, Mason returned to his hometown, Boise, and launched Benoit Marketing Consultants to continue his mission of helping small businesses flourish.</p>
                     </div>
