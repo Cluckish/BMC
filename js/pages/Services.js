2f5de772a4fbb569365c24b846f6b7fd605dcb4b
Cluckish <ethan.m.moore0@gmail.com>
2025-06-25 16:05:07 -0600

    Update Services.js

--- js/pages/Services.js
+++ js/pages/Services.js
@@ -1,1 +1,34 @@
-
+export function Services() {
+    return `
+        <div class="services-page dark-bg">
+            <div class="services-intro">
+                <h1>Our Services</h1>
+                <p>We offer flexible options for the highest quality services far cheaper than our competitors both locally and nationwide. Rest easy knowing you're getting what your business needs without paying for fluff services that take months to deliver.</p>
+            </div>
+            <div class="pricing-grid">
+                <div class="pricing-card">
+                    <h4>Complete Package</h4>
+                    <p class="price">$995</p>
+                    <ul>
+                        <li>Custom Branding, Business Cards, Pamphlets, and Style Guide</li>
+                        <li>Fully Tailored, Quality Website up to 5 Pages SEO Optimized</li>
+                        <li>Free Monthly Updates for the first 6 months</li>
+                        <li>Includes the First Year of Hosting and Domain Ownership</li>
+                    </ul>
+                    <a href="https://forms.clickup.com/9013811926/f/8cm7ppp-2233/8Q5YXIYVL7J99SZ3GX" target="_blank" rel="noopener noreferrer" class="cta-button">Start Today</a>
+                </div>
+                <div class="pricing-card">
+                    <h4>Full Consulting</h4>
+                    <p class="price variable">Variable</p>
+                     <ul>
+                        <li>Actively Managed Market Research & Competition Analysis</li>
+                        <li>Campaigns, Advertising, & Social Media Management</li>
+                        <li>Trend Analytics & AI Integration</li>
+                        <li>And More! Contact us for bespoke services that match your needs.</li>
+                    </ul>
+                    <a href="mailto:benoitmarketingconsultants@gmail.com" class="cta-button secondary">Contact Us</a>
+                </div>
+            </div>
+        </div>
+    `;
+}
